851220def22d05246e8e4a97129b1c8847c75929
jimouchen <jmc1625645320@gmail.com>
2020-08-14 22:06:10 +0800

    add and config the sqlite create db_table and show content of the table in web page

--- PythonWeb/DjangoProject/App/views.py
+++ PythonWeb/DjangoProject/App/views.py
@@ -1,7 +1,17 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-
 # Create your views here.
+from App.models import User
+
+
 def home(request):
-    return HttpResponse('首页')
+    # return HttpResponse('首页')
+    # 查询数据库
+    users = User.objects.all()
+    show_content = {
+        'title': 'Hello',
+        'name': '世界',
+        'users': users
+    }
+    return render(request, "index.html", context=show_content)
