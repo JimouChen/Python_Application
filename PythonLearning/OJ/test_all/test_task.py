aab103d91a54942472a333f00bcd6e108948a82a
jimouchen <jmc1625645320@gmail.com>
2020-09-29 01:19:25 +0800

    software test hw commit

--- PythonLearning/OJ/test_all/test_task.py
+++ PythonLearning/OJ/test_all/test_task.py
@@ -1,7 +1,46 @@
-n = int(input())
-s = 0
-for i in range(1, n+1):
-    if ('2' in str(i)) or ('0' in str(i)) or ('1' in str(i)) or ('9' in str(i)) and str(i)[0] != '0':
-        s += i
+'''求根'''
 
-print(s)
+while True:
+    try:
+        a, b, c = map(int, input('请输入A、B、C的值:').split())
+        if a > 200 or a < -200 or b > 200 or b < -200 or c > 200 or c < -200:
+            print('输入错误,请重新输入!')
+        else:
+            if a == 0:
+                if b == 0:
+                    if c == 0:
+                        print('方程AX^2 + BX + C = 0有无数个根')
+                    else:
+                        print('方程AX^2 + BX + C = 0 无根')
+                else:
+                    x = -c / b
+                    print('方程AX^2 + BX + C = 0有1个实根, x = %.2f' % x)
+            else:
+                # a 不等于0的情况
+
+                delta = b ** 2 - 4 * a * c
+                if delta > 0:
+                    x1 = (-b + (b ** 2 - 4 * a * c) ** 0.5) / (2 * a)
+                    x2 = (-b - (b ** 2 - 4 * a * c) ** 0.5) / (2 * a)
+                    print('方程AX^2 + BX + C = 0有2个不同的实根, x1 = %.2f，x2 = %.2f' % (x1, x2))
+
+                elif delta == 0:
+                    x = -b / (2 * a)
+                    print('方程AX^2 + BX + C = 0有2个相同的实根, x1 = x2 = %.2f' % x)
+                elif delta < 0:
+                    x1 = (-b + (b ** 2 - 4 * a * c) ** 0.5) / (2 * a)
+                    x2 = (-b - (b ** 2 - 4 * a * c) ** 0.5) / (2 * a)
+                    x1_real = round(x1.real, 2)
+                    x2_real = round(x2.real, 2)
+                    x_img = round(abs(x1.imag), 2)
+                    if x1.imag < 0:
+                        x1_imag = str(round(x1.imag))
+                    else:
+                        x1_imag = '+' + str(round(x1.imag))
+
+                    print(
+                        '方程AX^2 + BX + C = 0有2个不同的虚根, x1 = {}+{}j ，x2 = {}-{}j'.format(x1_real, x_img, x2_real, x_img))
+
+
+    except:
+        break
