70d9d3278b098a0d95da435f1cfd75a80e2e8e87
jimouchen <jmc1625645320@gmail.com>
2020-10-01 00:44:44 +0800

    tree def

---
+++ PythonLearning/DataStructure/Tree/tree_def.py
@@ -0,0 +1,12 @@
+"""
+# @Time    :  2020/9/30
+# @Author  :  Jimou Chen
+"""
+
+
+# 定义树节点
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
