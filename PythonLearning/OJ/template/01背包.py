4fe26b720b1f00d9c26bd5fc4ec88e7f006f8305
jimouchen <jmc1625645320@gmail.com>
2020-10-11 20:58:31 +0800

    update 01 backbag template

---
+++ PythonLearning/OJ/template/01背包.py
@@ -0,0 +1,22 @@
+"""
+# @Time    :  2020/10/11
+# @Author  :  Jimou Chen
+"""
+
+n, m = map(int, input().split())
+w = [0 for i in range(0, n+1)]
+v = [0 for i in range(0, n+1)]
+
+for i in range(1, n+1):
+    w[i], v[i] = map(int, input().split())
+
+dp = [[0 for i in range(m+1)] for j in range(n+1)]
+
+for i in range(1, n+1):
+    for j in range(1, m+1):
+        if w[i] > j:
+            dp[i][j] = dp[i-1][j]
+        else:
+            dp[i][j] = max(dp[i-1][j], dp[i-1][j-w[i]] + v[i])
+
+print(dp[n][m])
