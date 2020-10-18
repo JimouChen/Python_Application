225f0ae2c9b36ee7d9d204e5e2a32c1f010b8e41
jimouchen <jmc1625645320@gmail.com>
2020-10-18 14:22:42 +0800

    update  01背包，完全背包dp

--- PythonLearning/OJ/template/完全背包.py
+++ PythonLearning/OJ/template/完全背包.py
@@ -1,8 +1,7 @@
 """
-# @Time    :  2020/10/11
+# @Time    :  2020/10/18
 # @Author  :  Jimou Chen
 """
-
 n, m = map(int, input().split())
 w = [0 for _ in range(0, n + 1)]
 v = [0 for _ in range(0, n + 1)]
@@ -17,6 +16,6 @@ for i in range(1, n + 1):
         if w[i] > j:
             dp[i][j] = dp[i - 1][j]
         else:
-            dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - w[i]] + v[i])
+            dp[i][j] = max(dp[i - 1][j], dp[i][j - w[i]] + v[i])  # 这句与01背包不同
 
 print(dp[n][m])
