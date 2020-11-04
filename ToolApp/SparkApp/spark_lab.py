f6ab0580ba604f9f7d389dec74c50cd8ab1dce51
jmchen <jmc1625645320@gmail.com>
2020-11-04 16:43:01 +0800

    commit spark update lab unting Chinese words

--- ToolApp/SparkApp/spark_lab.py
+++ ToolApp/SparkApp/spark_lab.py
@@ -2,30 +2,28 @@ import jieba
 from pyspark import SparkContext
 
 if __name__ == '__main__':
-    # load file
-    with open('test.txt', 'r') as f:
+    # 读入文件
+    with open('hwzy.txt', 'r') as f:
         file = f.read()
 
     res_str = ''
     txt_str = ''
-    clear_list = ['\n', '。', '，', '， ', '！', '“', '”', '…', ' ', "'", '?', ':', '、']
-
+    clear_list = ['\n', '。', '，', '， ', '！', '“', '”', '…', ' ', "'", '?', '？', ':', '、', '：']
+    # 去掉文本中的标点符号和其他非文字
     for i in file:
         if i not in clear_list:
             res_str += i
 
     res_list = list((jieba.cut(res_str, cut_all=False)))
-    # print(res_list)
-
     sc = SparkContext('local', "WordCount")
     sc.setLogLevel("ERROR")
-
     rdd = sc.parallelize(res_list)
     res_dict = rdd.flatMap(lambda x: x.split(" ")).countByValue()
     res_dict = dict(sorted(res_dict.items(), key=lambda x: x[1], reverse=True))
-    # print(res_dict)
 
-    for key, value in res_dict.items():
-        print("%s %i" % (key, value))
-    # stop spark
+    with open('result.txt', 'w+')as save_file:
+        for key, value in res_dict.items():
+            save_file.write("%s %i\n" % (key, value))
+            print("%s %i" % (key, value))
+    # 停止 spark
     sc.stop()
