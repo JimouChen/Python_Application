80cd50d0c442387378d1f200f7832a9e8e2e6f93
jmchen <jmc1625645320@gmail.com>
2021-01-18 17:14:51 +0800

    test git update

--- TestExample/test_.py
+++ TestExample/test_.py
@@ -1,8 +1,1 @@
-def get_new_board(old_board: str, move: str):
-    for i in range(len(old_board) - 1):
-        if old_board[i:i + 2] == move[0:2] and i % 2 == 0:
-            return old_board[:i] + move[2:] + old_board[i + 2:]
-
-
-s = '8979695949392919097717866646260600102030405060708012720323436383'
-print(get_new_board(s, '7747'))
+print('hello')
