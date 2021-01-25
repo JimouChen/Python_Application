5bab92b61b022656ab80a6c5ac0859c510f9fb52
jmchen <jmc1625645320@gmail.com>
2021-01-25 17:46:25 +0800

    update and usage of scipy

---
+++ PythonLearning/MathematicalModeling/规划/scipy线性规划.py
@@ -0,0 +1,13 @@
+from scipy import optimize
+import numpy as np
+
+# 确定c,A,b,Aeq,beq
+c = np.array([2, 3, -5])
+A = np.array([[-2, 5, -1], [1, 3, 1]])
+B = np.array([-10, 12])
+Aeq = np.array([[1, 1, 1]])
+Beq = np.array([7])
+
+# 求解
+res = optimize.linprog(-c, A, B, Aeq, Beq)
+print(res)
